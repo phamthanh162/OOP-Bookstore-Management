ea71a8cfec74992a050b74c7f3707465520faae8
Panthervention <cheetosindustrial@gmail.com>
2026-04-28 14:40:34 +0700

    Chore: Resolve merge conflicts by removing build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_OOP_Bookstore_Management_FILE C:/Users/Cheetos/Documents/GitHub/OOP-Bookstore-Management/build/OOP_Bookstore_Management.exe)
-set(__QT_DEPLOY_TARGET_OOP_Bookstore_Management_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_OOP_Bookstore_Management_RUNTIME_DLLS C:/msys64/ucrt64/bin/Qt6Charts.dll;C:/msys64/ucrt64/bin/Qt6OpenGLWidgets.dll;C:/msys64/ucrt64/bin/Qt6Widgets.dll;C:/msys64/ucrt64/bin/Qt6OpenGL.dll;C:/msys64/ucrt64/bin/Qt6Gui.dll;C:/msys64/ucrt64/bin/Qt6Core.dll)
